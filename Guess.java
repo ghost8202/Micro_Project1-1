82a287f245b6610075c80cfda5dffdc7e3961732
ghost8202 <chrishall8202@hotmail.com>
2016-02-11 08:43:41 -0500

    answers

--- Guess.java
+++ Guess.java
@@ -5,15 +5,18 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Random;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class Guess extends JFrame {
+	String name, score, chance, winMes, loseMes, greatMes, lessMes;
 public void game(){
 	setTitle("Guess");
 	JFrame b = new JFrame();
 	FileDialog fd=new FileDialog(b,"pick a file:",FileDialog.LOAD);
-	fd.setFile("*.txt*");	//get only txt files
+	fd.setFile("*.*");	//get only txt files
 	fd.setVisible(true);
 	String path=fd.getDirectory();
 	String file=fd.getFile();
@@ -21,17 +24,13 @@ public void game(){
 		FileReader fr=new FileReader(path+"//"+file);
 		try {
 			BufferedReader br=new BufferedReader(fr);
-			String name=br.readLine();
-			while(br.readLine()!=null){
-				String score=br.readLine();
-				String chance=br.readLine();
-				String winMes=br.readLine();
-				String loseMes=br.readLine();
-				String greatMes=br.readLine();
-				String lessMes=br.readLine();
-				//System.out.println(name+score+chance+winMes);
-				//System.out.println(loseMes+greatMes+lessMes);
-			}
+			    name=br.readLine();
+				score=br.readLine();
+				chance=br.readLine();
+				winMes=br.readLine();
+				loseMes=br.readLine();
+				greatMes=br.readLine();
+				lessMes=br.readLine();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -41,4 +40,56 @@ public void game(){
 
 	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 }
+public void random(){ 
+	 int guessNum1,fin=0;
+	Random rnd = new Random(); 
+	int attempts = 0, k; 
+	int scr = 100;
+	main m=new main();
+	int rndNum = rnd.nextInt(100); 
+	 //System.out.println(rndNum);
+	while(attempts < 10&&fin!=1){ 
+		attempts++; 
+		scr = scr - 10; 
+		 
+		String guessNum = JOptionPane.showInputDialog(null, " Guess a number between 1 and 100.","",JOptionPane.CLOSED_OPTION);
+		try{
+		if(Integer.parseInt(guessNum)==JOptionPane.CLOSED_OPTION){
+			m.main(null);
+		}}catch(NumberFormatException e){
+			
+			if(guessNum!=null){guessNum1=0;}else{m.main(null);}
+		}
+		 try{
+		 guessNum1 = Integer.parseInt(guessNum); 
+		 }
+		 catch(NumberFormatException e){guessNum1=0;}
+		if(rndNum < guessNum1){ 
+                        k=JOptionPane.showConfirmDialog(null, greatMes+". "+score+scr,"", JOptionPane.CLOSED_OPTION);
+                        
+                    }else if(rndNum > guessNum1){
+                        k=JOptionPane.showConfirmDialog(null, lessMes + ". " + score + scr,"",JOptionPane.CLOSED_OPTION);
+                    }else { 
+                        k=JOptionPane.showConfirmDialog(null, winMes + ". " + score + scr,"",JOptionPane.CLOSED_OPTION);
+		}
+		
+		if(k==JOptionPane.CLOSED_OPTION){
+			m.main(null);
+        	System.exit(0);
+        }else{
+        	k=JOptionPane.showConfirmDialog(null, chance +(10-attempts),"",JOptionPane.CLOSED_OPTION);
+        	if(k==JOptionPane.CLOSED_OPTION){
+    			m.main(null);
+          	System.exit(0);
+          }
+        }
+	} 
+	
+      k=JOptionPane.showConfirmDialog(null, loseMes,"",JOptionPane.CLOSED_OPTION);
+      if(k==JOptionPane.CLOSED_OPTION){
+			m.main(null);
+      	System.exit(0);
+      }
+
+} 
 }
--- main.java
+++ main.java
@@ -1,9 +1,34 @@
 package game;
+/*
+ * Midterm Project 1 Part 1
+ * Chris Hall, Binny Verma, Camille Zapatos, Yooju Choi
+ * 
+ */
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
-public class main {
+public class main extends JFrame {
 	public static void main(String[] args){
-		
+
+JDialog.setDefaultLookAndFeelDecorated(true);
+int loop=0;
+while (loop!=1){
+int response = JOptionPane.showConfirmDialog(null, "Would you want to play the Guessing Game?", "Confirm",
+
+    JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+
+if (response == JOptionPane.NO_OPTION||response == JOptionPane.CLOSED_OPTION) {
+ // System.out.println("No button clicked or exit button hit");
+System.exit(0);
+} else if (response == JOptionPane.YES_OPTION) {
+
+ // System.out.println("Yes button clicked");
 	Guess g= new Guess();	
 	g.game();
-	}
+	g.random();
+
+}
+}
+}
 }
